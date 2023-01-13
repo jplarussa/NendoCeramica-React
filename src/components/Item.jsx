1adf36d8432663fbbc0856c37e4a826be4d6fed7
Jean Pierre La Russa <jplarussa@gmail.com>
2023-01-13 05:35:07 -0300

    Nuevo diseno de cards en Homepage (ItemListContainer)

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,27 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Item = ({ product }) => {
-
-    const { id, name, image, desc, price } = product
+    const { id, name, image, price } = product;
     return (
         <div className="col-md-6 p-3 ">
-            <div className="card p-3 h-100 d-flex align-items-center">
-                <div className="d-flex justify-content-between align-items-center mb-3">
-                    <div className="p-3">
-                        <h4 className="text-uppercase">{name}</h4>
+            <div className="wrapper">
+                <div className="containerN">
+                    <div className="top" style={{
+                        backgroundImage: `url(${image})`,
+                        backgroundRepeat: "no-repeat",
+                        backgroundPosition: "center",
+                        height: "80%",
+                        width: "100%",
+                        WebkitBackgroundSize: "100%",
+                        MozBackgroundSize: "100%",
+                        obackgroundSize: "100%",
+                        backgroundSize: "cover"
+                        }}>
                     </div>
-                    <div className="img-fluid">
-                        <img className="rounded" src={image} width="250" alt={name}/>
+                    <div className="bottom">
+                        <div className="left">
+                            <div className="details">
+                                <h4>{name}</h4>
+                                <h5>${price}</h5>
+                            </div>
+                            <div className="buy d-flex align-items-center justify-content-center">
+                                <Link to={"/item/"+ id}><button type="button" className="bg-nendo-light border-0"><img className="bg-nendo-light" src="/images/addBasket.svg" alt="Add to basket" width="60"/></button></Link>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="d-flex flex-column  mb-2">
-                    <p className="fs-6">{desc}</p>
-                    <h5 className="text-uppercase mb-0 flex mb-2">${price}</h5>
-                    <Link to={"/item/"+ id}><button type="button" className="btn bg-nendo-light link-dark w-50">Ver detalles</button></Link>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Item;
